f99867a29833ef683ca99d0ea2f294ab15a39f9d
v-peiranwang <v-peiranwang@microsoft.com>
2022-11-05 21:37:11 +0800

    add state B

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
 package org.example;
+import org.example.StateB;
 import org.example.StateA;
-
 public class Main {
-    public StateA endPointState = new StateA();
+    public StateB endPointState = new StateB();
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ src/main/java/org/example/StateB.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class StateB {
+    public Integer test_state_2 = 3;
+}
