2a951f33d3cf92f41cfa66e4864b9962d917407c
v-peiranwang <v-peiranwang@microsoft.com>
2022-11-08 19:33:29 +0800

    change static

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 import org.example.StateB;
 import org.example.StateA;
 public class Main {
-    public StateB endPointState = new StateB();
+    public static StateA endPointState = new StateA();
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
