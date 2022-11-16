12bfe190262f88a7976544c0c0daa6035d24762b
v-peiranwang <v-peiranwang@microsoft.com>
2022-11-16 13:11:22 +0800

    add state B

--- src/main/java/org/example/StateA.java
+++ src/main/java/org/example/StateA.java
@@ -3,4 +3,5 @@ package org.example;
 public class StateA {
     public Integer test_state_1 = 1;
     public Integer test_state_2 = 2;
+    public StateB test_state_b = new StateB();
 }
---
+++ src/main/java/org/example/StateB.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class StateB {
+    public int test_2 = 1;
+}
