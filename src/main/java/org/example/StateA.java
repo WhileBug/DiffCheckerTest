50f89f507593c738740d6002e6f809ee73db07df
v-peiranwang <v-peiranwang@microsoft.com>
2022-11-16 17:52:07 +0800

    add state_set

--- src/main/java/org/example/StateA.java
+++ src/main/java/org/example/StateA.java
@@ -1,11 +1,13 @@
 package org.example;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class StateA {
     public Integer test_state_1 = 1;
     public Integer test_state_2 = 2;
     public StateB test_state_b = new StateB();
-    public static Set<Integer> state_set = new HashSet<>();
+    public static Map<Integer, StateB> state_set = new HashMap<>();
 }
