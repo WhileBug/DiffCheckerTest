40624de86eeadfa877ec695ff892ac1a45690040
v-peiranwang <v-peiranwang@microsoft.com>
2022-11-16 17:48:50 +0800

    add state_set

--- src/main/java/org/example/StateA.java
+++ src/main/java/org/example/StateA.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class StateA {
     public Integer test_state_1 = 1;
     public Integer test_state_2 = 2;
     public StateB test_state_b = new StateB();
+    public static Set<Integer> state_set = new HashSet<>();
 }
